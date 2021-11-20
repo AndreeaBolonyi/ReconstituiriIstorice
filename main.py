b3c1924cef6225f658a48620c60d2449052107fc
Andreea Bolonyi <andreea.bolonyi@yahoo.com>
2021-11-20 12:57:04 +0200

    added age classification, reformat humerus.csv (nr_male ~= nr_female), convert ages to 5 classes

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication([])
     view = View(controller)
     view.show()
-    # bone = BoneModel("humerus", {'HML': 286, 'HEB': 65, 'HHD': 42.45, 'HMLD': 21.82, 'SEX': 0, 'AGE': '40-50'},
+    # bone1 = BoneModel("Humerus", {'HML': 286, 'HEB': 65, 'HHD': 42.45, 'HMLD': 21.82, 'SEX': 0, 'AGE': '40-50'},
     #                       'male', '40-50', "")
-    # bone = BoneModel("humerus", {'HML': 282,'HEB': 49, 'HHD': 36.99,'HMLD': 20.73, 'SEX': 1,'AGE': "50+"},
+    # bone2 = BoneModel("Humerus", {'HML': 282,'HEB': 49, 'HHD': 36.99,'HMLD': 20.73, 'SEX': 1,'AGE': "50+"},
     #                       'female', "50+", "")
 
     sys.exit(app.exec())
--- ml_alorithms/DecisionTree.py
+++ ml_alorithms/DecisionTree.py
@@ -1,10 +1,74 @@
 import csv
 
 import pandas as pd
+from tempfile import NamedTemporaryFile
+import shutil
 from matplotlib import pyplot as plt
-from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier, export_graphviz, export_text
 from sklearn.model_selection import train_test_split
+
+
+def convert_age(age):
+    if age.__contains__('+'):
+        aux = age.split('+')
+        if aux[0].__contains__("-"):
+            age = aux[0]
+        else:
+            aux[0] = int(aux[0])
+            if aux[0] <= 20:
+                return '<20'
+            elif 20 <= aux[0] <= 30:
+                return '20-30'
+            elif 30 <= aux[0] <= 40:
+                return '30-40'
+            elif 40 <= aux[0] <= 50:
+                return '40-50'
+            else:
+                return '50+'
+
+    if age.__contains__('-'):
+        ages = age.split('-')
+        min_age = int(ages[0])
+        max_age = int(ages[1])
+
+        if min_age <= 20:
+            return '<20'
+        elif 20 <= min_age and 30 >= max_age:
+            return '20-30'
+        elif 30 <= min_age and 40 >= max_age:
+            return '30-40'
+        elif 40 <= min_age and 50 >= max_age:
+            return '40-50'
+        else:
+            return '50+'
+
+    age = int(age)
+    if age <= 20:
+        return '<20'
+    elif 20 <= age <= 30:
+        return '20-30'
+    elif 30 <= age <= 40:
+        return '30-40'
+    elif 40 <= age <= 50:
+        return '40-50'
+    else:
+        return '50+'
+
+
+def modify_age(filename):
+    tempfile = NamedTemporaryFile('w+t', newline='', delete=False)
+
+    with open(filename, 'r', newline='') as csvFile, tempfile:
+        reader = csv.reader(csvFile, delimiter=',', quotechar='"')
+        writer = csv.writer(tempfile, delimiter=',', quotechar='"')
+
+        for row in reader:
+            new_age = convert_age(row[5])
+            row[5] = new_age
+            writer.writerow(row)
+
+    shutil.move(tempfile.name, filename)
 
 
 class DecisionTree:
@@ -18,28 +82,43 @@ class DecisionTree:
         self.__x_test = []
         self.__y_test = []
         self.__tree = None
+        self.__classes_name = []
 
     def import_data(self):
         if self.__bone.get_bone_type() == "Humerus":
             col_names = ['HML', 'HEB', 'HHD', 'HMLD', 'SEX', 'AGE']
             self.__feature_cols = ['HML', 'HEB', 'HHD', 'HMLD']
             self.__filename = "data/humerus.csv"
-            self.__data_set = pd.read_csv(self.__filename, header=None, names=col_names, nrows=100)
+
+            # modify_age(self.__filename)
+            self.__data_set = pd.read_csv(self.__filename, header=None, names=col_names)
         elif self.__bone.get_bone_type() == "Femur":
             col_names = ['FML', 'FML', 'FHD', 'FMLD', 'SEX', 'AGE']
             self.__feature_cols = ['FML', 'FML', 'FHD', 'FMLD']
             self.__filename = "data/femur.csv"
-            self.__data_set = pd.read_csv(self.__filename, header=None, names=col_names, nrows=100)
+            self.__data_set = pd.read_csv(self.__filename, header=None, names=col_names)
         else:
             raise Exception("type not found")
 
-        print("Dataset Length: ", len(self.__data_set))
-        print("Dataset Shape: ", self.__data_set.shape)
-        print("Dataset: ", self.__data_set.head())
-
-    def split_data(self):
+    def split_data_for_sex(self):
         x = self.__data_set[self.__feature_cols]
         y = self.__data_set.SEX
+        self.__classes_name = ['male', 'female']
+        self.__x_train, self.__x_test, self.__y_train, self.__y_test = train_test_split(x, y, test_size=0.2,
+                                                                                        random_state=1)
+
+    def convert_age_to_number(self, age):
+        index = 0
+        for x in self.__classes_name:
+            if x == age:
+                return index
+            else:
+                index += 1
+
+    def split_data_for_age(self):
+        x = self.__data_set[self.__feature_cols]
+        self.__classes_name = {*self.__data_set.AGE.tolist()}
+        y = [self.convert_age_to_number(x) for x in self.__data_set.AGE]
         self.__x_train, self.__x_test, self.__y_train, self.__y_test = train_test_split(x, y, test_size=0.2,
                                                                                         random_state=1)
 
@@ -58,49 +137,90 @@ class DecisionTree:
         new_features = {k: v for k, v in self.__bone.get_features().items() if k != "SEX" and k != "AGE"}
         new_features = pd.DataFrame(new_features, index=[0])
         pred = self.__tree.predict(new_features)
-        return pred[0]
+        return pred[0], self.__classes_name
 
     def accuracy(self, y_pred):
-        print("Confusion Matrix: ", confusion_matrix(self.__y_test, y_pred))
+        # print("Confusion Matrix: ", confusion_matrix(self.__y_test, y_pred))
         print("Accuracy : ", accuracy_score(self.__y_test, y_pred) * 100)
 
-    def solve(self):
-        self.import_data()
-        self.split_data()
+    def create_decision_tree(self, classification_type):
         self.train_using_gini()
         y_pred = self.predict()
 
-        print("Results using Gini Index:")
+        print("Results using Gini Index for " + classification_type + ":")
         self.accuracy(y_pred)
 
-        plot_data(self.__filename)
+    def solve_age(self):
+        self.import_data()
+        self.split_data_for_age()
+        self.create_decision_tree("age classification")
+
+        self.plot_data_age()
         export_graphviz(self.__tree,
-                        out_file="tree.dot",
+                        out_file="tree_age.dot",
+                        feature_names=self.__feature_cols,
+                        class_names=list(self.__classes_name),
+                        filled=True)
+
+        export_text(self.__tree, feature_names=self.__feature_cols)
+
+    def solve_sex(self):
+        self.import_data()
+        self.split_data_for_sex()
+        self.create_decision_tree("sex classification")
+
+        self.plot_data_sex()
+        export_graphviz(self.__tree,
+                        out_file="tree_sex.dot",
                         feature_names=self.__feature_cols,
                         class_names=['male', 'female'],
                         filled=True)
 
+        export_text(self.__tree, feature_names=self.__feature_cols)
 
-def plot_data(filename):
-    names = ['male', 'female']
-    height = [0, 0]
-    left = [1, 2]
+    def plot_data_sex(self):
+        names = self.__classes_name
+        height = [0, 0]
+        left = [1, 2]
 
-    with open(filename, 'r') as csvfile:
-        lines = csv.reader(csvfile, delimiter=',',)
-        k = 0
-        for row in lines:
-            if k == 100:
-                break
-            if row[4] == "0":
-                height[0] += 1
-            else:
-                height[1] += 1
-            k += 1
+        with open(self.__filename, 'r') as csvfile:
+            lines = csv.reader(csvfile, delimiter=',')
+            for row in lines:
+                if row[4] == "0":
+                    height[0] += 1
+                else:
+                    height[1] += 1
 
-    plt.bar(left, height, tick_label=names, width=0.8, color=['red', 'green'])
-    plt.xlabel('x-sex')
-    plt.ylabel('y-number')
-    plt.title(filename)
+        plt.bar(left, height, tick_label=names, width=0.8, color=['red', 'green'])
+        plt.xlabel('x-sex')
+        plt.ylabel('y-number')
+        plt.title(self.__filename)
 
-    plt.show()
+        plt.show()
+
+    def plot_data_age(self):
+        print(self.__classes_name)
+        names = list(self.__classes_name)
+        height = [0, 0, 0, 0, 0]
+        left = [1, 2, 3, 4, 5]
+
+        with open(self.__filename, 'r') as csvfile:
+            lines = csv.reader(csvfile, delimiter=',')
+            for row in lines:
+                if row[5] == '<20':
+                    height[0] += 1
+                elif row[5] == '20-30':
+                    height[1] += 1
+                elif row[5] == '30-40':
+                    height[2] += 1
+                elif row[5] == '40-50':
+                    height[3] += 1
+                elif row[5] == '50+':
+                    height[4] += 1
+
+        plt.bar(left, height, tick_label=names, width=0.8, color=['red', 'green', 'yellow', 'blue', 'orange'])
+        plt.xlabel('x-age')
+        plt.ylabel('y-number')
+        plt.title(self.__filename)
+
+        plt.show()
